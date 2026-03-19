e8b90b08226c4bf7f91c9bfa5eae3942c5761fb9
ethan <8bitbanana.ec@gmail.com>
2026-03-19 12:51:30 +0000

    Chatbox formatting

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,12 +2,26 @@ import { Component } from "preact";
 
 import * as css from "../pk.module.css";
 
+type ChatlogEntryProps = {
+    header: string,
+    message: string
+}
+
+class ChatlogEntry extends Component<ChatlogEntryProps> {
+    render() {
+        return <div className={css.chat_message_container}>
+            <span className={css.chat_header}>{this.props.header}</span>
+            <span className={css.chat_message}>{this.props.message}</span>
+        </div>
+    }
+}
+
 class Chatlog extends Component {
     render() {
         return <div id={css.chat_chatlog}>
             <h1>Chatlog</h1>
             <div>
-                <p>Wow</p>
+                <ChatlogEntry header="Mix:" message="Hello world!" />
             </div>
         </div>
     }
